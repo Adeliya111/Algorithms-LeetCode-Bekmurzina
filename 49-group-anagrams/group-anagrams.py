bbbd4d12bc42590de45d9ddba99935da2b3371ae
Adeliya111 <134287194+Adeliya111@users.noreply.github.com>
2026-03-20 23:58:12 +0600

    Time: 15 ms (45.55%) | Memory: 23.9 MB (32.82%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashmap = defaultdict(list)
+        for word in strs :
+            cnt = [0] * 26 #английские буквы
+            for ch in word :
+                cnt[ord(ch) - ord('a')] += 1 #возвращает unicode
+            
+            key = tuple(cnt)
+            hashmap[key].append(word)
+            
+        return list(hashmap.values())
