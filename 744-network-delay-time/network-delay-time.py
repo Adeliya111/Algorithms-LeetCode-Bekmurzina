ef490437ed8a33166c015fbc612be405ad5df713
Adeliya111 <134287194+Adeliya111@users.noreply.github.com>
2026-04-07 23:56:30 +0600

    Time: 321 ms (97.53%) | Memory: 21.2 MB (83.87%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.py
@@ -0,0 +1,27 @@
+from typing import List
+import heapq
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # Построение графа в виде списка смежности
+        graph = [[] for _ in range(n + 1)]
+        for u, v, w in times:
+            graph[u].append((v, w))
+        
+        # dist[i] = минимальное время от k до i
+        dist = [float('inf')] * (n + 1)
+        dist[k] = 0
+        heap = [(0, k)]  # (time, node)
+        
+        while heap:
+            time, node = heapq.heappop(heap)
+            if time > dist[node]:
+                continue
+            for nei, weight in graph[node]:
+                new_time = time + weight
+                if new_time < dist[nei]:
+                    dist[nei] = new_time
+                    heapq.heappush(heap, (new_time, nei))
+        
+        max_time = max(dist[1:])  # игнорируем индекс 0
+        return max_time if max_time != float('inf') else -1
