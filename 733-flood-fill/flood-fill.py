b400aa4d082400e89cb1a063f0a09d6d4e2c662d
Adeliya111 <134287194+Adeliya111@users.noreply.github.com>
2026-04-07 23:40:00 +0600

    Time: 0 ms (100.00%) | Memory: 19.4 MB (89.73%) - LeetSync

---
+++ 733-flood-fill/flood-fill.py
@@ -0,0 +1,21 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        oldColor = image[sr][sc]
+        if oldColor == newColor:
+            return image
+        
+        m, n = len(image), len(image[0])
+        queue = deque([(sr, sc)])
+        image[sr][sc] = newColor
+        
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+        
+        while queue:
+            r, c = queue.popleft()
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                # Проверка границ сетки и совпадения с исходным цветом
+                if 0 <= nr < m and 0 <= nc < n and image[nr][nc] == oldColor:
+                    image[nr][nc] = newColor  # Замена цвета перед добавлением в очередь
+                    queue.append((nr, nc))    # Добавление соседа для дальнейшего обхода
+        return image
