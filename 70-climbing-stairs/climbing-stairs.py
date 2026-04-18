f97ceceb2b85799147ce1e299d00ed0ba811fc91
Adeliya111 <134287194+Adeliya111@users.noreply.github.com>
2026-04-18 17:42:09 +0600

    Time: 0 ms (100.00%) | Memory: 19.3 MB (59.49%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -3,10 +3,10 @@ class Solution:
         if n <= 2:
             return n
         
-        prev2, prev1 = 1, 2  # dp[1] = 1, dp[2] = 2
+        prev2, prev1 = 1, 2  #ступени 1, 2
         
         for i in range(3, n + 1):
-            curr = prev1 + prev2  # dp[i] = dp[i-1] + dp[i-2]
+            curr = prev1 + prev2  
             prev2, prev1 = prev1, curr
         
         return prev1
